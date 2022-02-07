3cc9e9130298d31145f43aee83afdb53de1afb4a
M-JM <45374451+M-JM@users.noreply.github.com>
2022-02-07 12:32:45 +0100

    Write a function that takes an integer as input, and returns the number of bits that are equal to one in the binary representation of that number. You can guarantee that input is non-negative.
    Example: The binary representation of 1234 is 10011010010, so the function should return 5 in this case

---
+++ LeetCode/BitCounting.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public static class BitCounting
+    {
+        public static int CountBits(int n)
+        {
+            return Convert.ToString(n, 2).Where(x => x == '1').Count(); 
+        }
+
+    }
+}
